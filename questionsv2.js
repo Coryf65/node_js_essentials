64ca96194e94d7e47711309a7d068eaa1140e696
Cory <Coryf65@gmail.com>
2020-04-02 16:47:27 -0500

    pulled out functions into a seperate module!

--- questionsv2.js
+++ questionsv2.js
@@ -1,34 +1,11 @@
-// Recreating the Q&A quiz using our new readline module
-
-const readline = require("readline");
-
-// our interface
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// recreating this file using our new custom lib module
+const collectAnswers = require("./lib/collectAnswers");
 
 const questions = [
     "What is your name? ",
     "What is your favorite ice cream? ",
     "What will you build with Node js? "
 ];
-
-const collectAnswers = (questions, done) => {
-    const answers = [];
-    const [firstQuestion] = questions;
-
-    const questionAnswered = answer => {
-        answers.push(answer);
-        if (answers.length < questions.length) {
-            rl.question(questions[answers.length], questionAnswered);
-        } else {
-            done(answers);
-        }
-    };
-
-    rl.question(firstQuestion, questionAnswered);
-};
 
 // collect the answers from the user
 collectAnswers(questions, answers => {
