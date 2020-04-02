7d2bb13f28382cda08ddbb7d269a0b4222d3b508
Cory <Coryf65@gmail.com>
2020-04-02 11:58:18 -0500

    now using a require to load other modules

--- global.js
+++ global.js
@@ -1,3 +1,12 @@
+// each file we create is called a module, in order to load other modules we need to specify
+// with using 'require()'
+
+//loading external modules
+const path = require("path");
+
+console.log(`The filename is ${path.basename(__filename)}`);
+// The filename is global.js
+
 // playing with some global functions
 
 console.log(__dirname);
