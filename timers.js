ca502c1256979a19b1d50598d71fa8da5f43522d
Cory <Coryf65@gmail.com>
2020-04-02 14:21:46 -0500

    now clearing the interval

--- timers.js
+++ timers.js
@@ -1,8 +1,19 @@
 const waitTime = 3000;
+const waitInterval = 500;
+let currentTime = 0;
+
+const incTime = () => {
+    currentTime += waitInterval
+    console.log(`waiting ${currentTime / 1000} seconds`)
+}
 
 console.log(`setting a ${waitTime / 1000} second delay`);
 
 // one line arrow function
-const timerFinished = () => console.log("done");
+const timerFinished = () => {
+    clearInterval(Interval);
+    console.log("done");
+}
 
+const Interval = setInterval(incTime, waitInterval);
 setTimeout(timerFinished, waitTime);
