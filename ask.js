bd9bb2c031768dc14465b7ba5d690f0e14931e77
Cory <Coryf65@gmail.com>
2020-04-02 15:36:52 -0500

    added notes

--- ask.js
+++ ask.js
@@ -1,5 +1,7 @@
 const readline = require("readline");
 
+// We are creating our own Module, that inherits readline and using process.stdin and stdout for console input/output
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
