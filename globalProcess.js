b08c5d7d391bd008df55709b7e5832cd43dbd73c
Cory <Coryf65@gmail.com>
2020-04-02 12:08:05 -0500

    adding to the argv by adding additional args!

--- globalProcess.js
+++ globalProcess.js
@@ -6,3 +6,22 @@ console.log(process.pid);
 
 console.log(process.versions.node);
 // 8.11.4
+
+// collect info from the terminal, is an array
+console.log(process.argv);
+//[ 'C:\\Program Files\\nodejs\\node.exe',
+//'C:\\Users\\Cory\\Documents\\_Code\\node_js_essentials\\globalProcess' ]
+
+// now we are adding to the argv function
+// Run by passing in the args into terminal 
+// node globalProcess --greeting Hello --user Cory
+const grab = flag => {
+    let indexAfterFlag = process.argv.indexOf(flag) +1;
+    return process.argv[indexAfterFlag];
+};
+
+const greeting = grab("--greeting");
+const user = grab("--user");
+
+
+console.log(`${greeting} ${user}`);
