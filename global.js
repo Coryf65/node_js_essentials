1df186642850c49afae4c192550552184d93c9b1
Cory <Coryf65@gmail.com>
2020-04-02 11:54:53 -0500

    using some global functions

---
+++ global.js
@@ -0,0 +1,7 @@
+// playing with some global functions
+
+console.log(__dirname);
+// C:\Users\Cory\Documents\_Code\node_js_essentials
+
+console.log(__filename);
+// C:\Users\Cory\Documents\_Code\node_js_essentials\global.js
