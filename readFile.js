78d7027e0c66d2bf829d7d340a7ea63f7939cccc
Cory <Coryf65@gmail.com>
2020-04-03 10:35:55 -0500

    rewading a binary file and error handling

--- readFile.js
+++ readFile.js
@@ -2,7 +2,22 @@
 
 const fs = require("fs");
 
-const text = fs.readFile("./assets/loremipsum.txt", "UTF-8", (err, text) => {
+// reading a text file
+fs.readFile("./assets/loremipsum.txt", "UTF-8", (err, text) => {
+    if (err) {
+        console.log("oops an error occured: " + err.message);
+        process.exit();
+    }
     console.log("file contents read");
     console.log(text);
+});
+
+// reading a Binary File, don't need to suplly the encoding
+fs.readFile("./assets/funny_mug.jpg", (err, img) => {
+    if (err) {
+        console.log(`oops an error occured: ${err.message}`);
+        process.exit();
+    }
+    console.log("file contents read");
+    console.log(img);
 });
