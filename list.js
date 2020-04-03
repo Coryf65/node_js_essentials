1002bbd5c464c4d141b37cc72dec218e60a96fc3
Cory <Coryf65@gmail.com>
2020-04-03 10:24:57 -0500

    changed to read the dir async !

--- list.js
+++ list.js
@@ -2,7 +2,12 @@
 
 const fs = require("fs");
 
-// reading the contents, this runs sync
-const files = fs.readdirSync("./assets");
-
-console.log("contents of assests/ " + files)
+console.log("Started reading the Directory")
+// reading the contents, this runs Async
+const files = fs.readdir("./assets", (err, files) =>{
+    if (err) {
+        throw err;
+    }    
+    console.log("Complete")
+    console.log("contents of assests/ " + files)
+});
